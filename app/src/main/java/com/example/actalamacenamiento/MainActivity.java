8328c201b50f977cf370d3758ca635490b6e72e6
is-lgarcia <luis10.g@hotmail.com>
2020-02-24 12:49:37 -0600

    implementacion de logica de todos los componentes

--- app/src/main/java/com/example/actalamacenamiento/MainActivity.java
+++ app/src/main/java/com/example/actalamacenamiento/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.actalamacenamiento;
 
 import android.os.Bundle;
 
+import com.example.actalamacenamiento.dialogo.DialogoAbrir;
+import com.example.actalamacenamiento.dialogo.DialogoEliminar;
+import com.example.actalamacenamiento.modelo.ServicioArchivo;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,8 +14,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements DialogoAbrir.OnAbrirArchivoListener
+        , DialogoEliminar.OnEliminarArchivoListener {
+
+    private EditText editNombreArchivo, editTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +31,71 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editNombreArchivo = findViewById(R.id.edit_archivo);
+        editTexto = findViewById(R.id.edit_texto);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.abrir) {
-            return true;
+        switch (id){
+            case R.id.guardar:
+                if (!editNombreArchivo.getText().toString().equals("")
+                && !editTexto.getText().toString().equals("")){
+                    try {
+                        ServicioArchivo.guardar(this
+                                ,editNombreArchivo.getText().toString()+".txt"
+                                ,editTexto.getText().toString());
+                        editTexto.setText("");
+                        editNombreArchivo.setText("");
+                        Toast.makeText(this, "Archivo guardado Correctamente", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        Toast.makeText(this, "Error al guardar el archivo", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Rellene todas las cajas de texto", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.abrir:
+                DialogoAbrir abrir = new DialogoAbrir();
+                abrir.show(getSupportFragmentManager(),abrir.TAG);
+                break;
+            case R.id.borrar:
+                DialogoEliminar eliminar = new DialogoEliminar();
+                eliminar.show(getSupportFragmentManager(),eliminar.TAG);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAbrirArchivo(String nombreArchivo) {
+        try {
+            editTexto.setText(ServicioArchivo.cargar(this,nombreArchivo+".txt"));
+            editNombreArchivo.setText(nombreArchivo);
+            Toast.makeText(this, "El archivo fue abierto correctamente", Toast.LENGTH_SHORT).show();
+        }catch (IOException e){
+            Toast.makeText(this, "Error al cargar el archivo", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void OnEliminarArchivo(String nombreArchivo) {
+
+        if (ServicioArchivo.eliminar(this, nombreArchivo+".txt")){
+            Toast.makeText(this, "Archivo Eliminado", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "El archivo a eliminar no existe", Toast.LENGTH_SHORT).show();
+        }
     }
 }
